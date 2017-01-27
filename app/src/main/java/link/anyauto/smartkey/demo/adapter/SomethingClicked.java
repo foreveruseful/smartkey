bb0a1f6c965d0e34aa9b74a52151b85791c39994
discotek <foreveruseful@gmail.com>
2017-01-27 22:45:57 +0800

    Revert "first release"
    This reverts commit 51e00831857315ff824107695741af3b874788fc.

--- app/src/main/java/link/anyauto/smartkey/demo/adapter/SomethingClicked.java
+++
@@ -1,9 +0,0 @@
-package link.anyauto.smartkey.demo.adapter;
-
-/**
- * Created by discotek on 17-1-25.
- */
-
-public interface SomethingClicked {
-    void clicked(int position);
-}
--- apt/src/main/java/link/anyauto/smartkey/apt/manifestanalyzer/TargetApp.java
+++
@@ -1,9 +0,0 @@
-package link.anyauto.smartkey.apt.manifestanalyzer;
-
-import java.util.ArrayList;
-
-public class TargetApp {
-    public ArrayList<TargetDescriptor> activities = new ArrayList<>();
-    public ArrayList<TargetDescriptor> activityAliases = new ArrayList<>();
-    public ArrayList<TargetDescriptor> services = new ArrayList<>();
-}
